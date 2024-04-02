9fa29a0c7fb2d1a18c951a05af6cae7305ed24dd
vancy12 <saanvishet12@gmail.com>
2024-04-02 09:51:55 +0530

    sample main created

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
-#include "bitvector.c"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bloomfilter.h"
+
+int main(){
+    bloomFilter *filter = bloomFilterNewDefault(1024);
+    bloomFilterSetString(filter, "abc");
+    printf("%d\n", bloomFilterCheckString(filter, "abc"));
+    printf("%d\n", bloomFilterCheckString(filter, "bcd"));
+    printf("%d\n", bloomFilterCheckString(filter, "0"));
+    printf("%d\n", bloomFilterCheckString(filter, "1"));
+    bloomFilterSetString(filter, "2");
+    printf("%d\n", bloomFilterCheckString(filter, "2"));
+    return 0;
+}
